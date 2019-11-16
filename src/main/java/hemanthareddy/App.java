41dd4e069e9641f0f64533d96a0ea4a2740b4cf8
Hemantha <hemanth.beduduri@gmail.com>
2019-11-16 18:35:29 +0530

    new-repo

---
+++ src/main/java/hemanthareddy/App.java
@@ -0,0 +1,13 @@
+package hemanthareddy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
